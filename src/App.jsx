2f0cd17d0d360eb96a3e71bd2db0fb61b166585b
Sonu Kumar <krsonu025@gmail.com>
2026-08-24 15:53:10 +0530

    use jsx with curly braces

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,46 @@
-
 let name="sonu kumar";
 let age=31;
 
-function App()
+function fruit()
 {
+  return "Apple"
+}
+
+const user={
+  name:"sonu",
+  age:31,
+  address:"New Delhi"
+}
+
+let userArry=["amit","hello","kumar"];
+
+function operation(a,b,opt){
+  if(opt=='+')
+  {
+    return a+b;
+  }else if(opt=='-')
+  {
+    return a-b;
+  }else if(opt=='*')
+  {
+    return a*b;
+  }else if(opt=='/')
+  {
+    return a/b;
+  }
+}
+function App()
+{ 
   return(
     <>
     <h1>hello,i learn react js</h1>
     <p>Name : {name}</p>
     <p>Age : {age}</p>
+    <p>{name ? name : "name not found"}</p>
+    <p>{fruit()}</p>
+    <p>{user.name}</p>
+    <p>{userArry[0]}</p>
+    <p>{operation(10,2,'+')}</p>
     </>
   )
 }
