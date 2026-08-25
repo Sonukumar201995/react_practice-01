18acaa3ddd7563f452a06bbeafd57a64c58c4c98
Sonu Kumar <krsonu025@gmail.com>
2026-08-25 18:56:08 +0530

    show and hide (toggle) program

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
 import { useState } from "react";
-import Counter from "./Couter";
+// import Counter from "./Couter";
 
 function App()
 { 
  
-  const [fruit,setFruit]=useState("Apple");
+  const [display,setDisplay]=useState(true);
   
-  const handleFruit=()=>{
-    setFruit("Banana")
-  }
   return(
     <>
     <h1>hello,i learn State in Reactjs</h1>
-    <p>{fruit}</p>
-    <button onClick={handleFruit}>change</button>
-    <Counter/>
+    <button onClick={()=>setDisplay(!display)}>toggle</button>
+      {
+       display ? <h2>sonu kumar</h2> :null
+      }
     </>
   )
 }
--- src/Couter.jsx
+++ src/Couter.jsx
@@ -3,10 +3,13 @@ import { useState } from "react"
 const Counter=()=>{
    
     const [count,setCounter]=useState(0);
+    const handleCounter=()=>{
+        setCounter(count+1)
+    }
     return(
         <>
         <h1>counter :{count}</h1>
-        <button onClick={()=>setCounter(count+1)}>update</button>
+        <button onClick={handleCounter}>update</button>
         </>
     )
 }
