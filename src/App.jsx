1610af059b5bb22d440a13f7da10fc6ee0a8f4b2
Sonu Kumar <140957411+Sonukumar201995@users.noreply.github.com>
2026-09-05 18:19:19 +0530

    useEffect on both state

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  useEffect(() => {
-    console.log("Effect is running");
-  });
 
-  return <h1>Hello</h1>;
+  const [data,setData]=useState(0);
+  const [counter,setCounter]=useState(0);
+
+
+  useEffect(() => {
+    hello();
+  },[counter,data]);
+
+
+ 
+  function hello(){
+    console.log("hello is calling")
+  }
+
+  return(
+    <>
+    <h2>useEffect Hooks</h2>
+        <button onClick={()=>setCounter(counter+1)}>counter {counter}</button>
+        <button onClick={()=>setData(data+1)}>data {data}</button>
+    </>
+  ) 
 }
 
 export default App;
