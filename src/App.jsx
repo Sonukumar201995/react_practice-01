c16d99ee662dbdaaf8615656a2f7ece86e8262d9
Sonu Kumar <140957411+Sonukumar201995@users.noreply.github.com>
2026-09-14 14:40:49 +0530

    External css in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
+import './css/style.css'
+
 function App()
 {
   return(
     <>
-    <h1 style={{color:"red",backgroundColor:"pink"}}>inline css style in react js</h1>
+     <h1 className="heading">External style in react </h1>
+     <div className='container'>
+     <div>
+      <img className="image" src="https://www.w3schools.com/w3images/avatar2.png" alt="Avatar"/>
+      <div>
+        <h3>sonu kumar</h3>
+        <p>Software Engineer</p>
+      </div>
+     </div>
+      <div>
+      <img className="image" src="https://www.w3schools.com/w3images/avatar2.png" alt="Avatar"/>
+      <div>
+        <h3>monu kumar</h3>
+        <p>Deputy Manager</p>
+      </div>
+     </div>
+     </div>
     </>
   )
 }
