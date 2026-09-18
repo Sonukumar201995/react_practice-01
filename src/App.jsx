3546953ef8e1aa190887d3878180c62db434f0a9
Sonu Kumar <140957411+Sonukumar201995@users.noreply.github.com>
2026-09-18 14:08:08 +0530

    styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import UserProfile from "./userProfile";
-// import './css/style.css'
+import styled from "styled-components";
 
 function App()
 {
+
+  const Heading=styled.h1`
+  color:red`;
   return(
     <>
       <h1 className="heading">style with css modules</h1>
-      <UserProfile/>
-    </>
+      <Heading>hello world</Heading>
+       </> 
   )
 }
 export default App;
